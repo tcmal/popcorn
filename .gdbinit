989c4a726452a8dfb15b4ae6c3b1465f02f2b01d
Oscar Shrimpton <oscar.shrimpton.personal@gmail.com>
2020-01-10 18:52:58 +0000

    Add debugging capability to ./qemu.sh

---
+++ .gdbinit
@@ -0,0 +1,2 @@
+file kernel/popcorn.kernel
+target remote localhost:1234
--- qemu.sh
+++ qemu.sh
@@ -2,4 +2,4 @@
 set -e
 . ./build.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -kernel build/boot/prix.kernel
+qemu-system-$(./target-triplet-to-arch.sh $HOST) -kernel build/boot/popcorn.kernel -no-reboot -no-shutdown -s -S
