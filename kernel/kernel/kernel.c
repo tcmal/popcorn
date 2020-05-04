89673864d22836e2fa8f46fa4024570ed6e5c4b6
mal <oscar.shrimpton.personal@gmail.com>
2020-05-04 18:55:54 +0100

    move mbinfo page address and fix kernel_end_address being virtual

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
+#include <stdlib.h>
 #include <kernel/paging.h>
 #include <kernel/multiboot.h>
 #include <kernel/tty.h>
 #include <kernel/physm.h>
 
-const void* MBINFO_PAGE_ADDRESS = (void*) 0x42000000;
+const void* MBINFO_PAGE_ADDRESS = (void*) 0x01000000;
 extern int _kernel_end;
 
 void kernel_main(uint32_t* initialisedPageDirectory, mbInfo_t* physMbInfo) {
-	unsigned int kernel_end_address = (unsigned int) &_kernel_end;
+	unsigned int kernel_end_address = (unsigned int) &_kernel_end - 0xC0000000;
 
 	preparePaging(initialisedPageDirectory);
 	tty_init();
@@ -29,5 +30,6 @@ void kernel_main(uint32_t* initialisedPageDirectory, mbInfo_t* physMbInfo) {
 
 	physm_init(kernel_end_address);
 	
+
 	while (true) {};
 }
